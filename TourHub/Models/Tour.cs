d27a8b5d369256d2908956f81883327aba56bfb6
4L4M1N <mohammadamin601@gmail.com>
2019-08-02 00:32:24 +0600

    Added Tour Entity

---
+++ TourHub/Models/Tour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourHub.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public ApplicationUser Traveller { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Place { get; set; }
+        public decimal Cost { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
