7841883db9c47fa213a95c23b5585920a0b8aded
4L4M1N <mohammadamin601@gmail.com>
2019-08-01 22:15:15 +0600

    controller

--- TourHub/Controllers/HomeController.cs
+++ TourHub/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TourHub.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Description";
 
             return View();
         }
