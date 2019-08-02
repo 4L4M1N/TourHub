08e9205646da1cb4b4f2b6bb0d24f617a12be14c
4L4M1N <mohammadamin601@gmail.com>
2019-08-02 17:24:35 +0600

    Update TourGenre model

--- TourHub/Models/Tour.cs
+++ TourHub/Models/Tour.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,21 @@ namespace TourHub.Models
 {
     public class Tour
     {
+      
         public int Id { get; set; }
+
+        [Required]
         public ApplicationUser Traveller { get; set; }
         public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Place { get; set; }
+
+        [Required]
         public decimal Cost { get; set; }
+
+        [Required]
         public Genre Genre { get; set; }
     }
 }
