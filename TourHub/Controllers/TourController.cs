aa8a2e4043c4c501d9c2819e0f0c7e06687dc2ab
4L4M1N <mohammadamin601@gmail.com>
2019-08-03 18:47:30 +0600

    TourForm Added

--- TourHub/Controllers/TourController.cs
+++ TourHub/Controllers/TourController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TourHub.Models;
+using TourHub.ViewModels;
 
 namespace TourHub.Controllers
 {
     public class TourController : Controller
     {
         // GET: Tour
+        private readonly ApplicationDbContext _dbContext;
+        public TourController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new TourFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ TourHub/ViewModels/TourFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TourHub.Models;
+
+namespace TourHub.ViewModels
+{
+    public class TourFormViewModel
+    {
+        public string Place { get; set; }
+        public decimal Cost { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
