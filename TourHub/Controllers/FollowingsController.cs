90f6a56993d25fc795f103a16d5348f9e588c424
4L4M1N <mohammadamin601@gmail.com>
2019-08-15 16:20:40 +0600

    follower added

---
+++ TourHub/Controllers/FollowingsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TourHub.DTOs;
+using TourHub.Models;
+
+namespace TourHub.Controllers
+{
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDTO dto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_context.Followings.
+                Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Following already exists");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ TourHub/DTOs/FollowingDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourHub.DTOs
+{
+    public class FollowingDTO
+    {
+        public string FolloweeId { get; set; }
+    }
+}
