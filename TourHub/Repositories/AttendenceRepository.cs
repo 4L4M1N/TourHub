965ef503f168f84c25016e04033cd0a6e975fc41
4L4M1N <mohammadamin601@gmail.com>
2019-09-10 11:40:59 +0600

    Repository added

---
+++ TourHub/Repositories/AttendenceRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TourHub.Models;
+
+namespace TourHub.Repositories
+{
+    public class AttendenceRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+        public AttendenceRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Attendence> GetFutureAttendences(string userId)
+        {
+            return _context.Attendences
+                            .Where(a => a.AttendeeId == userId && a.Tour.DateTime > DateTime.Now)
+                            .ToList();
+        }
+    }
+}
---
+++ TourHub/Repositories/TourRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TourHub.Models;
+
+namespace TourHub.Repositories
+{
+    public class TourRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public TourRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Tour GetTourWithAttendees(int tourId)
+        {
+            return _context.Tours
+                .Include(a => a.Attendences.Select(s => s.Attendee))
+                .SingleOrDefault(t => t.Id == tourId);
+        }
+        public IEnumerable<Tour> GetTourUserAttending(string userId)
+        {
+            return _context.Attendences
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Tour)
+                .Include(t => t.Traveller)
+                .Include(g => g.Genre)
+                .ToList();
+        }
+    }
+}
